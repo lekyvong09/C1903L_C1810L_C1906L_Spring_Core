e790ce080977e9a58be6e5b52908f36e3177c39c
lekyvong09 <lekyvong09@gmail.com>
2021-10-15 21:05:31 +0700

    add aop & config

---
+++ src/com/ray/aop/SpringConfig.java
@@ -0,0 +1,12 @@
+package com.ray.aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.ray.aop")
+public class SpringConfig {
+
+}
